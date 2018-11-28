e01667cf40d985684154854b279d0e9964c57775
Ivan Smetanca <smetanca52ivan@gmail.com>
2018-11-28 16:04:11 +0200

    Improved style

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,9 @@
+def fibs(n)
+  arr = [0, 1]
+  arr << arr[-1] + arr[-2] until arr.length > n
+  arr.last
+end
 
-  def fibs(n)
-    arr = [0, 1]
-    arr << arr[-1] + arr[-2] until arr.length > n
-    arr.last
-  end
-
-  def fibs_rec(n)
-    n < 2 ? n : fibs_rec(n-1) + fibs_rec(n-2)
-  end
+def fibs_rec(n)
+  n < 2 ? n : fibs_rec(n - 1) + fibs_rec(n - 2)
+end
