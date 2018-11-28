f906ee800c4c5975151961d51d181a22d4440793
Ivan Smetanca <smetanca52ivan@gmail.com>
2018-11-28 15:59:16 +0200

    Improved style

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,13 +1,14 @@
-  def merge_sort(n)
-      return n if n.size <= 1
-      left,rigth = n.each_slice( (n.size/2.0).round ).to_a
-      merge(merge_sort(left), merge_sort(rigth))
-  end
+def merge_sort(arr)
+  return arr if arr.size <= 1
 
-  def merge(left, right)
-    sorted = []
-    until left.empty? || right.empty?
+  left, rigth = arr.each_slice((arr.size / 2.0).round).to_a
+  merge(merge_sort(left), merge_sort(rigth))
+end
+
+def merge(left, right)
+  sorted = []
+  until left.empty? || right.empty?
     left.first <= right.first ? sorted << left.shift : sorted << right.shift
-    end
-    sorted.concat(left).concat(right)
   end
+  sorted.concat(left).concat(right)
+end
